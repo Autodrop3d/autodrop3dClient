a53a092bfb72ed7575bc49f09d22cc87fbc7c2bf
Drew Gates <me@drewgates.com>
2017-09-02 00:09:31 -0400

    install script updates

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #! /bin/bash
 
 if [[ $EUID -ne 0 ]]; then
@@ -13,12 +12,37 @@ fi
 # define install location, bed_size(x,y,z), auto_ejection(true/false), material_type, material_color
 
 # install dependencies
-sudo apt-get install software-properties-common python-software-properties git
+sudo apt-get install software-properties-common python-software-properties git python-serial python-wxgtk2.8 python-pyglet python-numpy cython python-libxml2 python-gobject python-dbus python-psutil python-cairosvg libpython-dev figlet
 
 
 # install python
 
-# get Printrun from Github.
+# create directory for working files
+mkdir /autodrop
+cp -R . /autodrop/ #this won't work.
+cd /autodrop
+
+# get Printrun from Github and compile.
+git clone https://github.com/kliment/Printrun.git
+cd Printrun
+pip install -r requirements.txt
+python setup.py build_ext --inplace
+cd ..
+
+#clone and build WiringPi
+sudo apt purge wiringpi
+hash -r
+sudo apt update
+sudo apt upgrade
+cd /autodrop/
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+git pull origin
+./build
+
+
+
+#setup reboot at end of script
 cp start.sh /etc/init.d/autodrop-start.sh
 chmod 755 /etc/init.d/autodrop-start.sh
 sudo update-rc.d /etc/init.d/autodrop-start.sh defaults
