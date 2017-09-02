e39297f094fae8f0fc36e415de2083100e310586
mmiscool <mike@smbisoft.com>
2017-09-01 23:42:47 -0400

    Finished refactoring code to seperate directorys and making modular.

--- AutoDrop.sh
+++ AutoDrop.sh
@@ -2,14 +2,17 @@
 # include this boilerplate
 
 #These are all the settings for the printer. Change these 
-
+#date +'Script initialized at %Y-%m-%d-%H%M' >> /home/pi/3d.log
+cd /home/pi/Printrun
 printerServer='http://AUTODROP3D.COM/printerinterface/gcode'
-printerName='HARAMBE3D'
-printerColor='RED'
+printerName='drewsucks'
 printerMaterial='PLA'
-SIZEX='150'
-SIZEY='150'
-SIZEZ='150'
+SIZEX='100'
+SIZEY='100'
+SIZEZ='300'
+SERIALPORT='/dev/ttyS0'
+SERIALSPEED='76800'
+PRINTERSTYLE='blade-eject'
 
 
 function jumpto
@@ -26,53 +29,25 @@ clear
 figlet AutoDrop3d.com
 rm -f download.gcode
 wget -O download.gcode "$printerServer?name=$printerName&material=$printerMaterial&SizeX=$SIZEX&SizeY=$SIZEY&SizeZ=$SIZEZ"
-rm -f printpage.txt
-sed -n '2,10p' download.gcode >> printpage.txt
 
-echo $printerName >> printpage.txt
-echo "Printed on $(date)" >> printpage.txt
-
+#Check for the start instruction
 read -r PrintQueueInstruction<download.gcode
 
 if test "$PrintQueueInstruction" == ";start" ; then
 	clear
 	figlet Starting Print
+	PrintJobID=`sed -n '3p' download.gcode`
 	jumpto PrintThePart
 fi
 
-echo $PrintQueueInstruction
+figlet $PrintQueueInstruction
 sleep 10
 jumpto TheTop
 exit
 
 
 PrintThePart:
-PrintJobID=`sed -n '3p' download.gcode`
-
-#./printcore.py -b 115200 -v '/dev/ttyUSB0' start.gcode
-sleep 10
-#this is to print the file
-#lpr printpage.txt
-
-
-#CheckPrintStaus:
-#figlet Wating For Printer
-
-#PrintStatus="$(lpq -a)"
-#if test "$PrintStatus" != "no entries" ; then
-#	jumpto CheckPrintStaus
-#fi
-
-
-sleep 10
-./printcore.py -b 115200 -v '/dev/ttyUSB0' start.gcode
-sleep 25
-./printcore.py -b 115200 -v '/dev/ttyUSB0' download.gcode
-sleep 10
-./printcore.py -b 115200 -v '/dev/ttyUSB0' end.gcode
-
-
-#report Print Job Completed
-wget -O download.gcode "$printerServer?jobID=${PrintJobID:1}&stat=Done"
-source button-wait-and-go.sh
+cd $PRINTERSTYLE
+./print.sh
+cd ..
 jumpto TheTop
---
+++ prusa/print.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# include this boilerplate
+
+
+sleep 10
+./printcore.py -b $SERIALSPEED -v $SERIALPORT start.gcode
+sleep 25
+./printcore.py -b $SERIALSPEED -v $SERIALPORT download.gcode
+sleep 10
+./printcore.py -b $SERIALSPEED -v $SERIALPORT end.gcode
+
+
+#report Print Job Completed
+wget -O download.gcode "$printerServer?jobID=${PrintJobID:1}&stat=Done"
+source button-wait-and-go.sh
