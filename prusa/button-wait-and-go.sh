f13d671735468ab597d3de44b6dc4417c21b683f
Drew Gates <me@drewgates.com>
2017-09-01 23:35:37 -0400

    count updates while waiting for button push

--- prusa/button-wait-and-go.sh
+++ prusa/button-wait-and-go.sh
@@ -1,13 +1,25 @@
 #! /bin/bash
-
+COUNT=0
 while [ true ]
 do
-   CODE=`gpio -g read 23`
-   if [ $CODE -ge 1 ]; then
-      echo "kerblam"
+
+   COUNT=$((COUNT+1))
+   NEXT=`gpio -g read 23`
+   OOPS=`gpio -g read 24`
+   #success, start next
+   if [ $NEXT -ge 1 ]; then
+      echo "kerblam! Print was good! Let's start the next one!"
       break
    fi
-   echo "waiting..."
+   #this means it failed
+   if [ $OOPS -ge 1 ]; then
+      echo "kerblam! Success has kicked you in the face. The print job failed."
+      echo "It will now be requeued for another shot."
+      #INSERT CURL TO SET STATUS TO PRINT HERE
+      break
+   fi
+   echo "waiting...$COUNT"
+   
    sleep 0.1
 done
 
