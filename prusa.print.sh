170065f0e93bc2a23e1df0db09a546f1a26fb7cb
Drew Gates <me@drewgates.com>
2017-09-05 22:39:48 -0400

    post image upload.

--- prusa.print.sh
+++ prusa.print.sh
@@ -7,9 +7,23 @@ sleep 10
 sleep 10
 ./Printrun/printcore.py -b $SERIALSPEED -v $SERIALPORT "download.gcode"
 sleep 10
+
+#take photo of completed print
+fswebcam -r 640x480 --no-banner image.jpg
+
+#end gcode
 ./Printrun/printcore.py -b $SERIALSPEED -v $SERIALPORT "prusa.end.gcode"
 
+#report Print Job Completed and send photo of completed print to server
+new="image.jpg"
 
-#report Print Job Completed
-wget -O download.gcode "$printerServer?jobID=${PrintJobID:1}&stat=Done"
+curl -s \
+"$printerServer" \
+-F jobID=${PrintJobID:1} \
+-F stat=Done \
+-F content=$new \
+-F name=image \
+-F "filename=$new;image/jpeg;" \
+-F "image=@$new" -o download.gcode
+
 source button-wait-and-go.sh
