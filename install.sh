8c47d2888cfcf32ce3480f5dfdcb06ae9b81070a
Mmiscool <mike@smbisoft.com>
2018-10-15 18:32:50 -0400

    Added start of install script. We will see if it works.

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt-get install python3
+sudo apt-get install python3-pip
+
+
+sudo cp -a /. /autodrop3dClient/
+sudo chmod 755 -R /autodrop3dClient
+
+
+cd /autodrop3dClient
+pip3 install -r requirements.txt
+
+
+#Create the file for the service to be added
+sudo cp /autodrop3dClient/AD3D.service /lib/systemd/system/AD3D.service
+sudo systemctl enable AD3D
+sudo systemctl start AD3D
