540982f3a73019557158e16bfbee99dff2edc280
Drew Gates <7493966+drewgates@users.noreply.github.com>
2019-03-09 17:20:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
+# Install apt requirements.
+sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
 sudo apt-get install fswebcam
+sudo apt-get install python3-serial
 
-
-sudo cp -a /. /autodrop3dClient/
+#move service files to service location.
+sudo mkdir /autodrop3dClient/
+sudo cp -a . /autodrop3dClient/
 sudo chmod 755 -R /autodrop3dClient
 
-
+#move to service location and install pip requirements.
 cd /autodrop3dClient
 pip3 install -r requirements.txt
 pip3 install flask
-sudo apt install python-serial
 
-#Create the file for the service to be added
+#Create, Enable, and Start AD3D service.
 sudo cp /autodrop3dClient/AD3D.service /lib/systemd/system/AD3D.service
 sudo systemctl enable AD3D
 sudo systemctl start AD3D
