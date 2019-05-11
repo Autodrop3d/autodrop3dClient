cf08ad4415001625859f3b65a09b5eb891dbbb25
mmiscool <admin@penguin>
2019-05-11 22:37:18 +0000

    Updated install script to work updating unavailable package version numbers

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo apt-get install fswebcam
+sudo apt update
+sudo apt -y install git
+sudo apt -y install python3
+sudo apt -y install python3-pip
+sudo apt -y install fswebcam
 
 
 sudo cp -a . /autodrop3dClient/
 sudo chmod 755 -R /autodrop3dClient
 
-
+cd /
 cd /autodrop3dClient
 pip3 install -r requirements.txt
 
